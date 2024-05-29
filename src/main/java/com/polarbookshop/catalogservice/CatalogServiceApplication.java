f6f1c42a311d03d42f860931c4796dbe8b20e398
Gscript <msgeiger@gmail.com>
2024-05-28 21:24:42 -0400

    Chapter three coding

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
 
 }
