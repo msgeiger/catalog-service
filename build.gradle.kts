4a7a9f00c97710190e0489f398cd8df5e1e35afc
Gscript <msgeiger@gmail.com>
2024-05-30 07:25:12 -0400

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,21 @@
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 plugins {
     java
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
+}
+
+repositories {
+    mavenCentral()
+}
+
+ext {
+    set("springCloudVersion", "2023.0.1")
+}
+
+tasks.named<BootRun>("bootRun") {
+    systemProperty("spring.profiles.active", "testdata")
 }
 
 group = "com.polarbootshop"
@@ -18,9 +32,11 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webflux")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.withType<Test> {
