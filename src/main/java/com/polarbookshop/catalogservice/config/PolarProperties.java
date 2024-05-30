2f787271a281cc9d548ec6545ffd16be37ce2237
Gscript <msgeiger@gmail.com>
2024-05-30 07:23:29 -0400

    Properties exercise.

--- src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
+++ src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
@@ -1,2 +1,19 @@
-package com.polarbookshop.catalogservice.config;public class PolarProperties {
+package com.polarbookshop.catalogservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "polar")
+public class PolarProperties {
+    /**
+     * A message to welcome users.
+     */
+    private String greeting;
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
 }
